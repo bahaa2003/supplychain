a0b37b9c55534fb0413ec525b2238b5eb951e373
engMohammedMohsen <mohamedmohsan142@gmail.com>
2025-06-16 00:09:53 +0300

    refactor downloadInvoicePdf.controller using renderTemplate util

--- controllers/invoice/downloadInvoicePdf.controller.js
+++ controllers/invoice/downloadInvoicePdf.controller.js
@@ -1,43 +1,30 @@
-import puppeteer from 'puppeteer';
-import fs from 'fs-extra';
-import handlebars from 'handlebars';
-import path from 'path';
-import Invoice from '../../models/Invoice.js';
-import { AppError } from '../../utils/AppError.js';
+import puppeteer from "puppeteer";
+import Invoice from "../../models/Invoice.js";
+import renderTemplate from "../../utils/templateRenderer.js";
+import { AppError } from "../../utils/AppError.js";
 
 export const downloadInvoicePdf = async (req, res, next) => {
   try {
     const invoiceId = req.params.invoiceId;
-
-    const invoice = await Invoice.findById(invoiceId)
-      .populate('issuer receiver createdBy items.product');
-
+    const invoice = await Invoice.findById(invoiceId).populate(
+      "issuer receiver createdBy items.product"
+    );
     if (!invoice) {
-      return next(new AppError('Invoice not found', 404));
+      return next(new AppError("Invoice not found", 404));
     }
-
-    const templatePath = path.resolve('templates', 'invoice-template.html');
-    const htmlTemplate = await fs.readFile(templatePath, 'utf-8');
-
-    const template = handlebars.compile(htmlTemplate);
-    const html = template(invoice.toObject());
-
+    const html = renderTemplate("document", "invoice", invoice);
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.setContent(html, { waitUntil: 'networkidle0' });
-
+    await page.setContent(html, { waitUntil: "networkidle0" });
     const pdfBuffer = await page.pdf({
-      format: 'A4',
+      format: "A4",
       printBackground: true,
     });
-
     await browser.close();
-
     res.set({
-      'Content-Type': 'application/pdf',
-      'Content-Disposition': `attachment; filename=${invoice.invoiceNumber}.pdf`
+      "Content-Type": "application/pdf",
+      "Content-Disposition": `attachment; filename=${invoice.invoiceNumber}.pdf`,
     });
-
     res.send(pdfBuffer);
   } catch (err) {
     next(err);
