62b39c61a39c6849c00336c77ad25f3ff153413f
engMohammedMohsen <mohamedmohsan142@gmail.com>
2025-06-17 00:15:22 +0300

    bug fix in app (invoice router)

--- routes/invoice.routes.js
+++ routes/invoice.routes.js
@@ -1,12 +1,15 @@
-import { generateInvoiceFromOrder } from '../controllers/invoice/generateInvoiceFromOrder.controller.js';
-import { getCompanyInvoices } from '../controllers/invoice/getCompanyInvoices.controller.js';
-import { getSingleInvoice } from '../controllers/invoice/getSingleInvoice.controller.js';
-import { downloadInvoicePdf } from '../controllers/invoice/downloadInvoicePdf.controller.js';
+import { generateInvoiceFromOrder } from "../controllers/invoice/generateInvoiceFromOrder.controller.js";
+import { getCompanyInvoices } from "../controllers/invoice/getCompanyInvoices.controller.js";
+import { getSingleInvoice } from "../controllers/invoice/getSingleInvoice.controller.js";
+import { downloadInvoicePdf } from "../controllers/invoice/downloadInvoicePdf.controller.js";
 
-import { protectedRoute } from './../middleware/auth.middleware';
+import { protectedRoute } from "./../middleware/auth.middleware";
 
-router.post('/from-order/:orderId', protectedRoute, generateInvoiceFromOrder);
-router.get('/', protectedRoute, getCompanyInvoices);
-router.get('/:invoiceId', protectedRoute, getSingleInvoice);
-router.get('/:invoiceId/pdf', protectedRoute, downloadInvoicePdf);
+import express from "express";
+const router = express.Router();
 
+router.post("/from-order/:orderId", protectedRoute, generateInvoiceFromOrder);
+router.get("/", protectedRoute, getCompanyInvoices);
+router.get("/:invoiceId", protectedRoute, getSingleInvoice);
+router.get("/:invoiceId/pdf", protectedRoute, downloadInvoicePdf);
+export default router;
