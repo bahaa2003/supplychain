5bac4ab1321d8cae1e812685259fd34d76f259bd
unknown <mohammedmohsan142@gmail.com>
2025-10-06 12:38:02 +0300

    add message schema

---
+++ models/Message.schema.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const messageSchema = new Schema(
+  {
+    sender: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    receiver: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    content: { type: String, required: true },
+    isCreate: { type: Boolean, default: false },
+    isDeliver: { type: Boolean, default: false },
+    isRead: { type: Boolean, default: false },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Message", messageSchema);
